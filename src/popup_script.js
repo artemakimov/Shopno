32b35e49c1be811c05b754a45808ee9ee7abd7db
artemakimov <86406541+artemakimov@users.noreply.github.com>
2022-07-14 14:22:23 +0300

    Add files via upload

--- src/popup_script.js
+++ src/popup_script.js
@@ -1,39 +1,16 @@
-// Get the modal
+
 let modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-let img = document.getElementById("myImg");
-let img2 = document.getElementById("myImg2");
-let img3 = document.getElementById("myImg3");
-let img4 = document.getElementById("myImg4");
-let img5 = document.getElementById("myImg5");
-let img6 = document.getElementById("myImg6");
 let modalImg = document.getElementById("img01");
-let captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-}
-img2.onclick = function(){
+const images = document.querySelectorAll('img.featured-works__pic');
+
+images.forEach(img => {
+  img.addEventListener('click' , (event) =>{
+    console.log(event.target.src)
     modal.style.display = "block";
-    modalImg.src = this.src;
-}
-img3.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-  }
-img4.onclick = function(){
-      modal.style.display = "block";
-      modalImg.src = this.src;
-}
-img5.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-}
-img6.onclick = function(){
-      modal.style.display = "block";
-      modalImg.src = this.src;
-}
+    modalImg.src = event.target.src;
+  })
+})
 
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
