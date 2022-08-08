156dd3b224221cbbd13b973e1a62a77e23c11a50
artemakimov <86406541+artemakimov@users.noreply.github.com>
2022-08-08 10:39:31 +0300

    Add files via upload

---
+++ js/popup_script.js
@@ -0,0 +1,20 @@
+
+let modal = document.getElementById("myModal");
+
+let modalImg = document.getElementById("img01");
+const images = document.querySelectorAll('img.featured-works__pics-flex_item_pic');
+
+images.forEach(img => {
+  img.addEventListener('click' , (event) =>{
+    modal.style.display = "block";
+    modalImg.src = event.target.src;
+  })
+})
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
